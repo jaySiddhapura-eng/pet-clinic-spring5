0301f5bcbb03acb5a0ae7b00f564481e69af1fdb
Siddhapura <jay.siddhapura@cgi.com>
2020-09-02 10:51:50 +0200

    data pom modified

---
+++ pet-clinic-data/src/main/java/CGI/springframework/petclinic/model/petType.java
@@ -0,0 +1,13 @@
+package CGI.springframework.petclinic.model;
+
+public class petType {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ pet-clinic-web/src/main/java/CGI/springframework/petclinic/model/Vet.java
@@ -0,0 +1,5 @@
+package CGI.springframework.petclinic.model;
+
+public class Vet extends Person{
+
+}
