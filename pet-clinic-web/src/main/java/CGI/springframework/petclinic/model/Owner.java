e660d7dbe1553bced65ea2b1e591ef5f83122509
Siddhapura <jay.siddhapura@cgi.com>
2020-09-02 12:14:11 +0200

    [maven-release-plugin] prepare release pet-clinic-0.0.1

--- pet-clinic-web/src/main/java/CGI/springframework/petclinic/model/Owner.java
+++
@@ -1,5 +0,0 @@
-package CGI.springframework.petclinic.model;
-
-public class Owner extends Person {
-
-}
--- pet-clinic-web/src/main/java/CGI/springframework/petclinic/model/Vet.java
+++
@@ -1,5 +0,0 @@
-package CGI.springframework.petclinic.model;
-
-public class Vet extends Person{
-
-}
